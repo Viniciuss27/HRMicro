c56b0d9d4f13c419fd26f039b794c8a21adf41bc
vinicius <viniciusdejjeuspessoal@gmail.com>
2026-05-31 11:15:53 -0300

    Hr-Oauth criado

---
+++ Hr-Oauth/src/main/java/vinix/HrOauthApplication.java
@@ -0,0 +1,15 @@
+package vinix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HrOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrOauthApplication.class, args);
+	}
+
+}
