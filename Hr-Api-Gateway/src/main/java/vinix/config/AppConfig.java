3430b0a82221cca98101ecdf71cd5bd0fe4118d3
vinicius <viniciusdejjeuspessoal@gmail.com>
2026-06-03 16:29:49 -0300

    autenticação com JWT concluida

---
+++ Hr-Api-Gateway/src/main/java/vinix/config/AppConfig.java
@@ -0,0 +1,25 @@
+package vinix.config;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Configuration
+public class AppConfig {
+
+    @Value("${security.jwt.secret}")
+    private String secret;
+
+    @Bean
+    SecretKey jwtSecretKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(
+            java.util.Base64.getEncoder().encodeToString(secret.getBytes())
+        );
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
